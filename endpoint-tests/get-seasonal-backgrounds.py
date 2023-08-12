10972bb720cc1cf413b42948ae1065d6f5d6179c
trustytrojan <tarbot24@hotmail.com>
2023-08-11 21:53:34 -0400

    changes

--- endpoint-tests/get-seasonal-backgrounds.py
+++ endpoint-tests/get-seasonal-backgrounds.py
@@ -1,12 +1,16 @@
-from osuExchange.seasonalbg import get_seasonal_backgrounds
+from osuExchange import OsuApiClient
+from json import JSONDecoder
 
-access_token = open("token", "r").read()
+client = OsuApiClient(**JSONDecoder().decode(open('client-info.json').read()))
 
 user_id = 8908337
 beatmap_id = 939209
 
 print(f'Fetching scores for user with id {user_id} on beatmap {beatmap_id}')
 
-bgs = get_seasonal_backgrounds(access_token)
+bgs = client.get_seasonal_backgrounds()
 
-print(f"A link to a Seasonal Background: {bgs.backgrounds[1].url}")
+for bg in bgs.backgrounds:
+    print(bg.url)
+
+#print(f"A link to a Seasonal Background: {bgs.backgrounds[1].url}")
